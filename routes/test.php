df7917a650db75f52298fafec0a8a6481930f06a
Flavio <flavio.huanacchiri@gmail.com>
2020-12-02 20:28:36 -0500

    Fix: Nuevo modelo y ruta test

--- routes/test.php
+++ routes/test.php
@@ -1,1 +1,15 @@
 <?php
+
+use App\Business;
+Route::get("test-business", function() {
+	try {
+		$obj = new Business;
+		$obj->number_identifer = "10435350378";
+		$obj->name = "Fernando Jacobo Quiroz Cabanillas";
+		$obj->save();
+
+		echo "Exito";
+	} catch (Exception $e) {
+		echo $e->getMessage();
+	}
+});
