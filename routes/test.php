aa70235b9b0d1c262d228e65523313fb9e7d6156
Lucía Andrea Quinto Ascurra <lquinto@isil.isil.edu.pe>
2020-12-05 19:45:24 -0500

    update: test.php

--- routes/test.php
+++ routes/test.php
@@ -4,8 +4,8 @@ use App\Business;
 Route::get("test-business", function() {
 	try {
 		$obj = new Business;
-		$obj->number_identifer = "10435350378";
-		$obj->name = "Fernando Jacobo Quiroz Cabanillas";
+		$obj->number_identifer = "10724465120";
+		$obj->name = "Lucia Andrea Quinto Ascurra";
 		$obj->save();
 
 		echo "Exito";
