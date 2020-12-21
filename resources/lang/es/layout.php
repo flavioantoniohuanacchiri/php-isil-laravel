17fefd3e71bc68e1537c71f81be69336265b6a45
Joseph Jesus Jimenez Sanchez <jjimenez@isil.isil.edu.pe>
2020-12-21 00:09:10 -0500

    cambio coma layout español

--- resources/lang/es/layout.php
+++ resources/lang/es/layout.php
@@ -4,15 +4,15 @@ return [
 	"sidebar" => [
 		"master" => "Mantenimientos",
 		"user" => "Usuarios",
-		"profile" => "Perfiles"
-		"product" => "Productos"
-		"category" => "Categorias"
-		"attribute" => "Atributos"
-		"attributetype" => "Tipo Atributos"
-		"producthasattribute" => "Producto Atributos"
-		"client" => "Clientes"
-		"provider" => "Proveedores"
-		"sale" => "Ventas"
+		"profile" => "Perfiles",
+		"product" => "Productos",
+		"category" => "Categorias",
+		"attribute" => "Atributos",
+		"attributetype" => "Tipo Atributos",
+		"producthasattribute" => "Producto Atributos",
+		"client" => "Clientes",
+		"provider" => "Proveedores",
+		"sale" => "Ventas",
 		"saledetail" => "Detalles Ventas"
 
 
