170b7ad6c663810731bc8b99f0bf6fa6245b0b3f
Katia Elizabeth Garvich San Martin <kgarvich@isil.isil.edu.pe>
2020-12-21 20:34:21 -0500

    sql

---
+++ sql/Atributo.sql
@@ -0,0 +1,14 @@
+/*
+-- Query: Select * from atributo
+LIMIT 0, 1000
+
+-- Date: 2020-12-21 20:15
+*/
+INSERT INTO `` (`id`,`codigo`,`nombre`,`estado`,`atributotipo_id`,`created_at`,`updated_at`,`deleted_at`) VALUES (1,'CA001','Largo','1',2,'2020-12-19 20:05:28','2020-12-20 01:33:07',NULL);
+INSERT INTO `` (`id`,`codigo`,`nombre`,`estado`,`atributotipo_id`,`created_at`,`updated_at`,`deleted_at`) VALUES (2,'CA002','Ancho','1',2,'2020-12-19 20:05:28',NULL,NULL);
+INSERT INTO `` (`id`,`codigo`,`nombre`,`estado`,`atributotipo_id`,`created_at`,`updated_at`,`deleted_at`) VALUES (3,'CA003','Alto','1',2,'2020-12-19 20:05:28',NULL,NULL);
+INSERT INTO `` (`id`,`codigo`,`nombre`,`estado`,`atributotipo_id`,`created_at`,`updated_at`,`deleted_at`) VALUES (4,'CA004','Fecha','1',3,'2020-12-19 20:05:28',NULL,NULL);
+INSERT INTO `` (`id`,`codigo`,`nombre`,`estado`,`atributotipo_id`,`created_at`,`updated_at`,`deleted_at`) VALUES (5,'CA005','Hora','1',3,'2020-12-19 20:05:28',NULL,NULL);
+INSERT INTO `` (`id`,`codigo`,`nombre`,`estado`,`atributotipo_id`,`created_at`,`updated_at`,`deleted_at`) VALUES (6,'CA006','Textura','1',1,'2020-12-19 20:05:28',NULL,NULL);
+INSERT INTO `` (`id`,`codigo`,`nombre`,`estado`,`atributotipo_id`,`created_at`,`updated_at`,`deleted_at`) VALUES (7,'CA007','Material','1',1,'2020-12-19 20:05:28','2020-12-20 01:15:53',NULL);
+INSERT INTO `` (`id`,`codigo`,`nombre`,`estado`,`atributotipo_id`,`created_at`,`updated_at`,`deleted_at`) VALUES (9,'CA008','Garantia','1',3,'2020-12-20 14:34:58','2020-12-20 14:35:47','2020-12-20 14:35:47');
